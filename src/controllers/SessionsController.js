da9b5ea674455a2bbed1438a870d9b8b6a05c5a9
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-04-25 01:09:42 -0300

    [Davi] Validating registered user

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,10 +1,22 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 class SessionsController {
 
     // Função para criar uma seção
     async create(request, response) {
         const { email, password } = request.body;
-        
-        return response.json({ email, password})
+
+        // Buscando um usuário pelo email
+
+        // O 'first' garante que traga só um usuário com esse email.
+        const user = await knex("users").where({ email }).first();
+
+        if(!user){
+            throw new AppError("E-mail e/ou senha incorreta", 401);
+        }
+
+        return response.json(user);
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const { Router } = require('express');
 const usersRoutes = require('./users.routes');
 const notesRoutes = require('./notes.routes');
 const tagsRoutes = require('./tags.routes');
-const sessionsRoutes = require("./sessions.routes");
+const sessionsRoutes = require('./sessions.routes');
 
 const routes = Router();
 
